1ca5ff97ec12c743b2b8354e7dc569d445d09d14
plasmas <plasticplasmas@outlook.com>
2020-08-12 13:50:34 +0800

    rewrite App component as function with hooks

--- src/App.js
+++ src/App.js
@@ -1,75 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// const App = () => {
+const App = () => {
 
-//   const [TDList, setTDList] = useState([
-//     {content: 'Buy milk', iscomplete: false},
-//     {content: 'get newspaper', iscomplete: true},
-//     {content: 'go for a walk', iscomplete: true},
-//   ]);
+  const [TDList, setTDList] = useState([
+    {content: 'Buy milk', iscomplete: false},
+    {content: 'get newspaper', iscomplete: true},
+    {content: 'go for a walk', iscomplete: true},
+  ]);
 
-//   const removeHandler = (i) => {
-//     setTDList(TDList.splice(i,1))
-//   }
 
-//   const createHandler = () => {
-
-//   }
-
-//   console.log(TDList);
-//   return (
-//     <div className="App">
-//       <div className="App-header">You have {TDList.length} To-Do items</div>
-//       <List TDList={TDList} removeHandler={removeHandler} />
-//       <CreateBox createHandler={createHandler} />
-//     </div>
-//   )
-// }
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      TDList: [
-        {content: 'Buy milk', iscomplete: false},
-        {content: 'get newspaper', iscomplete: true},
-        {content: 'go for a walk', iscomplete: true},
-      ],
-      version: 1.3
-    };
-    this.removeHandler = this.removeHandler.bind(this);
-    this.createHandler = this.createHandler.bind(this);
+  const removeHandler = (index) => {
+    TDList.splice(index, 1);
+    setTDList([...TDList]);
   }
 
-  removeHandler(i) {
-    let TDList = this.state.TDList;
-    TDList.splice(i, 1);
-    let newList = TDList;
-    console.log(newList);
-    this.setState({
-      TDList: newList,
-      version: 1.4
-    })
-    console.log(this.state)
-  }
-
-  createHandler() {
+  const createHandler = () => {
 
   }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">You have {this.state.TDList.length} To-Do items</div>
-        <List TDList={this.state.TDList} removeHandler={this.removeHandler} />
-        <CreateBox createHandler={this.createHandler} />
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <div className="App-header">You have {TDList.length} To-Do items</div>
+      <List TDList={TDList} removeHandler={removeHandler} />
+      <CreateBox createHandler={createHandler} />
+    </div>
+  )
 }
-
 
 const List = prep => {
 
